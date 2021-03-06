b386b59d062f4b1ca170c2379110eb7d23998aec
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-03-06 10:48:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(390,200);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    fill(0, 255, 213);
+    stroke(117, 8, 212);
+    strokeWeight(10);
+    rect(50,50,540,380,20,20,20,20);
+    image(video,120,120,400,240);
+}
+function take_snapshot(){
+    save("selfie.png");
+}
