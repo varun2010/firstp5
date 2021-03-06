6885383e0edef5d05851637e7ea168b22caca75f
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-03-06 15:11:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,9 +12,6 @@ function draw(){
     stroke(117, 8, 212);
     strokeWeight(10);
     rect(50,50,540,380,20,20,20,20);
-    fill(228, 232, 16);
-    stroke(17, 214, 20);
-    circle(50,50,40);
     image(video,120,120,400,240);
 }
 function take_snapshot(){
